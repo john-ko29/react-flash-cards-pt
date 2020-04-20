b0c86c217a8e5de0808b18d4f62c2a9cbbbb6a32
John Ko <jko121790@gmail.com>
2020-04-20 16:42:45 -0700

    reset method in create-card componement complete

--- src/components/create-card.jsx
+++ src/components/create-card.jsx
@@ -1,25 +1,37 @@
 import React from 'react';
 
-function CreateCard(props) {
-  return (
-    <div>
-      <h1 className='text-center'>Create New Card</h1>
-      <div className="row d-flex justify-content-center">
-        <form className='d-flex flex-column col-8'>
-          <label htmlFor="question">Question:</label>
-          <textarea name="question" id="question" cols="30" rows="5" placeholder="Put a question">
-          </textarea>
-          <label htmlFor="answer">Answer:</label>
-          <textarea name="answer" id="answer" cols="30" rows="5" placeholder="Put an answer">
-          </textarea>
-          <div className="row justify-content-end">
-            <button className="btn-danger m-2" type='reset'>Cancel</button>
-            <button className="btn-primary m-2" type='submit'>Save Card</button>
-          </div>
-        </form>
+class CreateCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.reset = this.reset.bind(this);
+  }
+
+  reset() {
+    this.props.setView('view-cards');
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className='text-center'>Create New Card</h1>
+        <div className="row d-flex justify-content-center">
+          <form className='d-flex flex-column col-8' onReset={this.reset}>
+            <label htmlFor="question">Question:</label>
+            <textarea name="question" id="question" cols="30" rows="5" placeholder="Put a question">
+            </textarea>
+            <label htmlFor="answer">Answer:</label>
+            <textarea name="answer" id="answer" cols="30" rows="5" placeholder="Put an answer">
+            </textarea>
+            <div className="row justify-content-end">
+              <button className="btn-danger m-2" type='reset'>Cancel</button>
+              <button className="btn-primary m-2" type='submit'>Save Card</button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default CreateCard;
