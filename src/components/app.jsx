8af22586aedcee882f5eda6eed90cf2ce416073e
John Ko <jko121790@gmail.com>
2020-04-20 14:59:46 -0700

    three components and imported complete

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
+import ViewCards from './view-cards';
+import ReviewCards from './review-cards';
+import CreateCard from './create-card';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      view: 'view-cards'
+    };
+  }
+
+  setView(location) {
+    this.setState({ view: location });
+  }
+
   render() {
     return (
       <h1 className='text-center'>Flash Card App</h1>
