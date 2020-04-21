1be33297aa27b5114464cc1e0ca120ae91b1f5c0
John Ko <jko121790@gmail.com>
2020-04-20 18:19:47 -0700

    feature 3 complete

--- src/components/view-cards.jsx
+++ src/components/view-cards.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
 
 function ViewCards(props) {
+  console.log(props);
+  const flashCards = props.cards.map((card, index) => {
+    return (
+      <div key={index}>
+        <div className="card p-3">
+          <blockquote className="blockquote bg-dark text-white mb-0 card-body">
+            <header className="blockquote-header">
+              Question:
+            </header>
+            <p>{card.question}</p>
+          </blockquote>
+          <blockquote className="blockquote bg-secondary text-white mb-0 card-body">
+            <header className="blockquote-header">
+              Answer:
+            </header>
+            <p>{card.answer}</p>
+          </blockquote>
+        </div>
+      </div>
+    );
+  });
   return (
-    <h1 className='text-center'>My Cards</h1>
+    <div>
+      <h1 className='text-center'>My Cards</h1>
+      {flashCards}
+    </div>
   );
 }
 
