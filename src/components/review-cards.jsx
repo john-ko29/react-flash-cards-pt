9af95b88576e4bb34d0523154c2f889d40d4828e
John Ko <jko121790@gmail.com>
2020-04-20 19:34:40 -0700

    previous and next card complete

--- src/components/review-cards.jsx
+++ src/components/review-cards.jsx
@@ -4,12 +4,27 @@ class ReviewCards extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      index: 0,
       isFront: true
     };
+    this.nextCard = this.nextCard.bind(this);
+    this.previousCard = this.previousCard.bind(this);
   }
 
   componentDidMount() {
     this.props.setActiveCard(0);
+  }
+
+  nextCard() {
+    this.setState(card => ({
+      index: this.state.index + 1
+    }));
+  }
+
+  previousCard() {
+    this.setState(card => ({
+      index: this.state.index - 1
+    }));
   }
 
   render() {
